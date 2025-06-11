688688231882ed8195c1e1f10e4c1722a0cb126e
rakibulhasan044 <rhrakib044@gmail.com>
2025-06-11 17:39:00 +0600

    home page, all blogs, and blog details page

---
+++ src/app/blogs/[blogId]/page.tsx
@@ -0,0 +1,28 @@
+import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5001/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5001/blogs/${blogId}`);
+  const blog = await res.json();
+  return (
+    <div className="my-10">
+      <BlogDetailsCard blog={blog} />
+    </div>
+  );
+};
+
+export default BlogDetailsPage;
